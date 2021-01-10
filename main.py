f0550ea94a054dc8e28fe2bd6e8ec937d63b9d00
nxthingco <76076144+nxthingco@users.noreply.github.com>
2021-01-10 19:44:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,8 +6,5 @@ with open('2.txt','r') as file:
 	file.close()
 with open('result.txt','w') as out:
 	for x in content_1.split(' '):
-		print(x)
 		for y in content_2.split(' '):
-			print(y)
-			if x == y:
-				out.write(x)
+			if x == y: out.write(x)
